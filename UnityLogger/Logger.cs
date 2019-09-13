667c48469c10f7018ad067b784126f0b1cd5c857
José M. Nieves <jose.nieves@gmail.com>
2019-09-13 10:58:59 -0500

    Improving logger.

--- UnityLogger/Logger.cs
+++ UnityLogger/Logger.cs
@@ -17,12 +17,22 @@ namespace UnityLogger
 
     public static class Logger
     {
-        public static LogType LogLevel { get; set; }
+        private static LogType _logLevel;
 
-        public static void Log(object message)
+        public static LogType LogLevel
+        {
+            get => _logLevel;
+            set
+            {
+                _logLevel = value;
+                Debug.unityLogger.logEnabled = LogLevel != LogType.None;
+            }
+        }
+
+        public static void Log(object message, string hexColor = "#acafb2")
         {
             if ((LogLevel == LogType.All || LogLevel == LogType.Message) && LogLevel != LogType.None)
-                Log(message, LogType.Message);
+                Log(message, LogType.Message, hexColor);
         }
 
         public static void LogWarning(object message)
@@ -47,30 +57,32 @@ namespace UnityLogger
         public static void LogException(Exception exception)
         {
             if ((LogLevel == LogType.All || LogLevel == LogType.Exception) && LogLevel != LogType.None)
-                Log("", LogType.Exception, exception);
+                Log("", LogType.Exception, "", exception);
         }
 
-        private static void Log(object message, LogType logType, Exception exception = null)
+        private static void Log(object message, LogType logType, string hexColor = "#acafb2", Exception exception = null)
         {
             if (Debug.unityLogger.logEnabled)
+            {
                 switch (logType)
                 {
                     case LogType.Message:
-                        Debug.Log(message);
+                        Debug.Log(Debug.isDebugBuild ? $"<color={hexColor}>{message}</color>\n" : message);
                         break;
                     case LogType.Warning:
-                        Debug.LogWarning($"<color={"#FFFF00"}>{message}</color>");
+                        Debug.LogWarning(Debug.isDebugBuild ? $"<color=#ADFF2F>{message}</color>\n" : message);
                         break;
                     case LogType.Error:
-                        Debug.LogError($"<color={"#FF0000"}>{message}</color>");
+                        Debug.LogError(Debug.isDebugBuild ? $"<color=#FF0000>{message}</color>\n" : message);
                         break;
                     case LogType.Assertion:
-                        Debug.LogAssertion($"<color={"#FF0000"}>{message}</color>");
+                        Debug.LogAssertion(Debug.isDebugBuild ? $"<color=#FF0000>{message}</color>\n" : message);
                         break;
                     case LogType.Exception:
                         Debug.LogException(exception);
                         break;
                 }
+            }
         }
     }
 }
